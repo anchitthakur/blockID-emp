1ddc3e5c790b36594ee57acde647fed28f288a66
anchit <anchit.thakur11@gmail.com>
2021-05-13 22:19:10 +0530

    added utils

--- utils/graphql-client.js
+++ utils/graphql-client.js
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 const endpoint = 'https://graphql.fauna.com/graphql';
 
 export const graphQLClient = (token) => {
-    const secret = process.env.NEXT_PUBLIC_FAUNA_KEY;
+    const secret = token || process.env.NEXT_PUBLIC_FAUNA_GUEST_SECRET;
 
     return new GraphQLClient(endpoint, {
         headers: {
